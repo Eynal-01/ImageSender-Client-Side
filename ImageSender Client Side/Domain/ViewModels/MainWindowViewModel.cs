dc337a6bef68e1fd558614c9fbe22bb382391c8c
Baxsh_cw07@itstep.edu.az <Baxsh_cw07@itstep.edu.az>
2023-07-28 09:49:51 +0400

    Add project files.

---
+++ ImageSender Client Side/Domain/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ImageSender_Client_Side.Commands;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace ImageSender_Client_Side.Domain.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public RelayCommand SelectImageCommand { get; set; }
+        public RelayCommand SendImageCommand { get; set; }
+        public Image Image { get; set; }
+
+        public MainWindowViewModel()
+        {
+            SelectImageCommand = new RelayCommand((obj) =>
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+            });
+        }
+    }
+}
