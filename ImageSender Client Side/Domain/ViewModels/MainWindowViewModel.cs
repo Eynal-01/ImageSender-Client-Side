521c494e1754a180a460e1884ef802c9300e4423
Baxsh_cw07 <Baxsh_cw07@STHQ0129-12>
2023-07-28 13:03:43 +0400

    Image added main window successfully

--- ImageSender Client Side/Domain/ViewModels/MainWindowViewModel.cs
+++ ImageSender Client Side/Domain/ViewModels/MainWindowViewModel.cs
@@ -8,21 +8,59 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+using System.Windows.Markup;
+using System.Collections.ObjectModel;
 
 namespace ImageSender_Client_Side.Domain.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BaseViewModel
     {
         public RelayCommand SelectImageCommand { get; set; }
         public RelayCommand SendImageCommand { get; set; }
-        public Image Image { get; set; }
+
+        private BitmapImage image;
+
+        public BitmapImage Image
+        {
+            get { return image; }
+            set { image = value; OnPropertyChanged(); }
+        }
+
+        public bool IsOkay { get; set; } = false;
+        public byte[] Data { get; set; }
 
         public MainWindowViewModel()
         {
             SelectImageCommand = new RelayCommand((obj) =>
             {
-                OpenFileDialog openFileDialog = new OpenFileDialog();
+                File_send(obj);
             });
+        }
+        public void File_send(object parametr)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Image"; // Default file name
+            dlg.DefaultExt = ".png"; // Default file extension
+            //dlg.Filter = "Image Files (.jpg)|*.jpg;*.jpeg;*.png;*.gif;*.tif;"; // Filter files by extension
+
+            Nullable<bool> result = dlg.ShowDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                Image = new BitmapImage(new Uri(dlg.FileName));
+                ImageBrush brush = new ImageBrush(Image);
+                //_MW.Background = brush;
+
+                JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(Image));
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    encoder.Save(ms);
+                    Data = ms.ToArray();
+                }
+                IsOkay = true;
+            }
         }
     }
 }
